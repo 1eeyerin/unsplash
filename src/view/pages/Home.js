2dc025ce9ac9705822a9a1137dd86fab20b976aa
ye-r1 <ye_r1@daum.net>
2021-02-20 15:48:19 +0900

    검색 컴포넌트, 페이지의 추가

---
+++ src/view/pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+import Visual from "../components/Home/Visual";
+
+function Home(){
+    
+    return(
+        <Container>
+            <Visual />
+        </Container>
+    )
+}
+
+const Container = styled.div`
+
+`
+
+export default Home;
