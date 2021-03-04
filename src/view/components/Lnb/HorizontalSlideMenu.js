47d307e634e08cbd06b3011b9428740767c3b368
lee ye-rin <40863185+ye-r1@users.noreply.github.com>
2021-03-04 15:36:10 +0900

    Revert "초기 setState 오류"

--- src/view/components/Lnb/HorizontalSlideMenu.js
+++ src/view/components/Lnb/HorizontalSlideMenu.js
@@ -1,60 +1,36 @@
-import React, {useRef, useState, useEffect} from "react";
+import React, {useRef, useState} from "react";
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 import {SlideBtnNext, SlideBtnPrev} from "../../../icons";
 import cn from "classnames";
-import {scrollMenu} from "../../../lib/Common";
+import {moveSlideMenu} from "../../../lib/Common";
 
 function HorizontalSlideMenu({topicNav}) {
 
-    const Ref = useRef();
-    const [maxScroll, setMaxScroll] = useState(0);
-    const [scrollLeft, setScrollLeft] = useState(0);
-    const [className, setClassName] = useState("");
+    const slideRef = useRef();
+    const [breakPoint, setBreakPoint] = useState("");
 
-    useEffect(() => {
-        setMaxScroll(Ref.current?.scrollWidth - Ref.current?.clientWidth);
-    }, [Ref.current?.scrollWidth]);
-
-    useEffect(() => {
-        handleClassName();
-    }, [scrollLeft, maxScroll]);
-
-    const {onClickLeft, onClickRight, handleClassName} = scrollMenu({
-        maxScroll,
-        scrollLeft,
-        setClassName,
-        setScrollLeft
-    });
-
+    const {lnbMenu} = moveSlideMenu({slideRef, setBreakPoint});
 
     return (
-        <Container className={cn(className)}>
-            {
-                scrollLeft > 0 &&
-                <Button
-                    type="button"
-                    className="btn prev"
-                    onClick={onClickLeft}>
-                    <SlideBtnPrev/>
-                    <span>prev button</span>
-                </Button>
-            }
-            <List ref={Ref} scrollLeft={scrollLeft}>
+        <Container className={cn(breakPoint)}>
+            <Button
+                type="button"
+                className="btn prev"
+                onClick={() => lnbMenu("prev")}>
+                <SlideBtnPrev/><span>prev button</span>
+            </Button>
+            <List ref={slideRef}>
                 {
-                    topicNav.map((item, i) => <li key={i}><Link to={`/t/${item.slug}`}>{item.title}</Link></li>)
+                    topicNav.map((item, i) => <li><Link to={`/t/${item.slug}`}>{item.title}</Link></li>)
                 }
             </List>
-            {
-                scrollLeft < maxScroll &&
-                <Button
-                    type="button"
-                    className="btn next"
-                    onClick={onClickRight}>
-                    <SlideBtnNext/>
-                    <span>next button</span>
-                </Button>
-            }
+            <Button
+                type="button"
+                className="btn next"
+                onClick={() => lnbMenu("next")}>
+                <SlideBtnNext/><span>next button</span>
+            </Button>
         </Container>
     )
 }
@@ -67,6 +43,7 @@ const Container = styled.div`
 
   &:after, &:before {
     transition: opacity .2s ease-in-out;
+    opacity: 0;
     content: "";
     display: block;
     pointer-events: none;
@@ -80,20 +57,15 @@ const Container = styled.div`
   &:before {
     left: 0;
     background: linear-gradient(270deg, hsla(0, 0%, 100%, 0) 0, #fff 95%, #fff);
+    opacity: 0;
   }
 
   &:after {
     right: 0;
     background: linear-gradient(90deg, hsla(0, 0%, 100%, 0) 0, #fff 95%, #fff);
+    opacity: 1;
   }
 
-  &.prev:before {
-    opacity: 0;
-  }
-
-  &.next:after {
-    opacity: 0;
-  }
 
   .btn {
     border: 0;
@@ -102,6 +74,7 @@ const Container = styled.div`
     position: absolute;
     top: 50%;
     transform: translateY(-50%);
+    outline: 0;
     cursor: pointer;
     z-index: 2;
     width: 40px;
@@ -111,6 +84,7 @@ const Container = styled.div`
     &.prev {
       left: 0;
       margin-left: 8px;
+      opacity: 0;
     }
 
     &.next {
@@ -123,16 +97,48 @@ const Container = styled.div`
     }
   }
 
+  &.prev .prev {
+    opacity: 0;
+  }
+
+  &.prev:before {
+    opacity: 0;
+  }
+
+  &.next .prev {
+    opacity: 1;
+  }
+
+  &.next:before {
+    opacity: 1;
+  }
+
+  &.next .next {
+    opacity: 0;
+  }
+
+  &.next:after {
+    opacity: 0;
+  }
+
+  &.active .btn {
+    opacity: 1;
+  }
+
+  &.active:before, &.active:after {
+    opacity: 1;
+  }
+
 `
 const Button = styled.button`
 `
 const List = styled.ul`
   white-space: nowrap;
+  overflow-x: scroll;
+  overflow-y: hidden;
   height: 56px;
   font-size: 14px;
   margin-left: -32px;
-  transition: transform .4s;
-  transform: ${(props) => `translateX(-${props.scrollLeft}px)`};
 
   &::-webkit-scrollbar {
     display: none;
