eeb5600e6e95d2f23c2fe1f59f916cfc53ea1f25
ye-r1 <ye_r1@daum.net>
2021-03-04 17:23:00 +0900

    getTopicData 데이터 연결

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,7 @@ const API = {
     getPhotos: (data) => FetchJson.get('/photos', data),
     searchPhotos: (data) => FetchJson.get('/search/photos', data),
     getTopicList: (data) => FetchJson.get('/topics', data),
+    getTopicPhoto: (data) => FetchJson.get(`/topics/${data.query}/photos`, data),
 }
 
 export default API;
