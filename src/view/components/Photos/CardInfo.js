e754aecead57980378c42b4c84c105c332832b76
ye-r1 <ye_r1@daum.net>
2021-03-06 06:18:48 +0900

    photo card hover 컴포넌트 만들기

--- src/view/components/Photos/CardInfo.js
+++ src/view/components/Photos/CardInfo.js
@@ -1,17 +1,102 @@
 import React from "react";
 import styled from "styled-components";
+import {IconPhotoCardAdd, IconPhotoCardDownload, IconPhotoCardLike} from "../../../icons";
 
-function CardInfo(){
-    
-    return(
+function CardInfo({name, profile_image, urls}) {
+
+    return (
         <Container>
-            CardInfo
+            <TopUtilBox>
+                <Button><IconPhotoCardLike/></Button>
+                <Button><IconPhotoCardAdd/></Button>
+            </TopUtilBox>
+
+            <BottomUtilBox>
+                <UserAvatar>
+                    <UserInfo>
+                        <UserImage><img src={profile_image.small} alt=""/></UserImage>
+                        <p><span className="e_">{name}</span></p>
+                    </UserInfo>
+                </UserAvatar>
+                <Button as="a" href={urls.full} download><IconPhotoCardDownload/></Button>
+            </BottomUtilBox>
         </Container>
     )
 }
 
 const Container = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 1;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  opacity: 0;
 
+  &:hover {
+    opacity: 1;
+  }
 `
 
+const Button = styled.button`
+  height: 32px;
+  padding: 0 11px;
+  font-size: 14px;
+  line-height: 30px;
+  color: #767676;
+  background-color: hsla(0, 0%, 100%, .9);
+  border: 1px solid transparent;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.06);
+  -webkit-border-radius: 4px;
+  -moz-border-radius: 4px;
+  border-radius: 4px;
+  text-align: center;
+  user-select: none;
+  cursor: pointer;
+  outline: 0;
+
+  svg {
+    position: relative;
+    top: -1px;
+    width: 15px;
+    fill: #767676;
+  }
+
+  & + button {
+    margin-left: 8px;
+  }
+`
+const TopUtilBox = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`
+const BottomUtilBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+const UserAvatar = styled.div`
+  display: flex;
+  flex: 1;
+`
+const UserInfo = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+
+  p {
+    color: #fff;
+    display: flex;
+    flex: 1;
+  }
+`
+const UserImage = styled.div`
+  border-radius: 50%;
+  overflow: hidden;
+  background-color: rgba(0, 0, 0, .1);
+  margin-right: 8px;
+`
 export default CardInfo;
