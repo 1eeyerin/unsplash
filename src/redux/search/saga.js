2c0564da6caf2d78d1ad0fe74ad565630afeedfd
ye-r1 <ye_r1@daum.net>
2021-03-09 03:49:20 +0900

    메인, 검색 컨테이너에 무한스크롤 기능 추가하기

--- src/redux/search/saga.js
+++ src/redux/search/saga.js
@@ -1,23 +1,31 @@
-import {all, takeLatest, call, put} from "redux-saga/effects";
+import {all, takeLatest, call, put, select} from "redux-saga/effects";
 import {Action} from "./redux";
 import API from "../../api";
 
 const saga = function* () {
     yield all([
         takeLatest(Action.Types.SEARCH_PHOTOS, function* ({data}) {
+            yield put(Action.Creators.updateState({isLoading : true}))
+            const {searchResults} = yield select(state => state.search);
+
             try {
-                yield put(Action.Creators.updateState({
-                    searchResults: {},
-                }))
                 const result = yield call(API.searchPhotos, data);
                 if(result) {
                     yield put(Action.Creators.updateState({
-                        searchResults: result,
+                        searchResults: {
+                            ...result,
+                            results: [
+                                ...searchResults.results,
+                                ...result.results,
+                            ]
+                        }
                     }))
                 }
             } catch (e) {
                 console.log('@@ e',e);
             }
+
+            yield put(Action.Creators.updateState({isLoading : false}))
         })
     ])
 }
---
+++ src/view/components/InfiniteScroll/index.js
@@ -0,0 +1,29 @@
+import React, {useEffect} from "react";
+import styled from "styled-components";
+import {useOnViewport} from "../../../hooks/useOnViewport";
+
+function InfiniteScroll({children, getMoreItems = () => {}, isLoading}) {
+
+    const [ref, inView] = useOnViewport([{
+        rootMargin: "300px",
+    }, isLoading]);
+
+    useEffect(() => {
+        inView && getMoreItems();
+    }, [inView]);
+
+    return (
+        <>
+            {children}
+            <Sentinel ref={ref}/>
+        </>
+    )
+}
+
+const Sentinel = styled.div`
+  position: absolute;
+  bottom: 0;
+  height: 50px;
+`
+
+export default InfiniteScroll;
