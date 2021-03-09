ca23b78991b32d59be0f8a4590a16f7f64f2e570
ye-r1 <ye_r1@daum.net>
2021-03-09 18:27:57 +0900

    Photolist color 스켈레톤 만들기

--- src/view/components/Photos/PhotoList.js
+++ src/view/components/Photos/PhotoList.js
@@ -3,15 +3,10 @@ import styled from "styled-components";
 import PhotoCard from "./PhotoCard";
 import {recomposePhotos} from "../../../lib/Common";
 import {photosActions} from "../../../redux/ActionCreators";
-import _ from "lodash";
-import PhotoListSkeleton from "../Loader/PhotoListSkeleton";
 
 function PhotoList({data}) {
 
     const photosGroup = recomposePhotos(data);
-
-
-    if (_.isEmpty(data)) return <PhotoListSkeleton/>;
 
     const onClick = (id) => {
         photosActions.getPhotoById(id);
