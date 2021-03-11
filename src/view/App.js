05c63a588d74ba8a2f9bd6bcabc64e86011f41ee
ye-r1 <ye_r1@daum.net>
2021-03-12 03:42:34 +0900

    #46 popup photo 컨테이너 추가

--- src/view/App.js
+++ src/view/App.js
@@ -1,30 +1,25 @@
-import React from 'react'
-import styled from 'styled-components';
+import React from 'react';
 import {GlobalStyle} from "../styled/Reset.Styled";
 import Routes from "./routes/Routes";
 import HeaderContainer from "./containers/HeaderContainer";
 import {useSelector} from "react-redux";
-import LayerPopup from "./components/PopupPhoto/LayerPopup";
+import PopupPhotoContainer from "./containers/PopupPhotoContainer";
 
 const App = () => {
 
     const {popupPhoto} = useSelector(state => state.photos);
 
     return (
-        <Container>
+        <>
             <GlobalStyle/>
             <HeaderContainer/>
             <Routes/>
             {
                 popupPhoto.show &&
-                <LayerPopup/>
+                <PopupPhotoContainer/>
             }
-        </Container>
+        </>
     )
 }
-
-const Container = styled.div`
-
-`;
 
 export default App;
