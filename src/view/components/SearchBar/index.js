ba0c0e5ef4028930315c052b03c598e85e373a00
ye-r1 <ye_r1@daum.net>
2021-03-18 16:33:13 +0900

    isActivePath 만들기, search bar 생성

---
+++ src/view/components/SearchBar/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+function SearchBar() {
+  return <Container>index</Container>;
+}
+
+const Container = styled.div``;
+
+SearchBar.propTypes = {
+  a: PropTypes.shape({
+    b: PropTypes.string
+  })
+};
+
+export default SearchBar;
