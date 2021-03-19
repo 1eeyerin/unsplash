a9179c86e6ae2cffb26244bdddded6a584824643
ye-r1 <ye_r1@daum.net>
2021-03-19 18:02:29 +0900

    #66 검색바 컴포넌트 추가

--- src/view/components/SearchBar/index.js
+++ src/view/components/SearchBar/index.js
@@ -1,17 +1,41 @@
 import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
+import { font } from "../../../styled/Font";
+import SearchMenu from "./SearchMenu";
+import SearchControl from "./Util";
+import { splitLastPath } from "../../../lib/Common";
 
-function SearchBar() {
-  return <Container>index</Container>;
+function SearchBar({ location, search }) {
+  const splitPath = splitLastPath("/s/", location?.pathname);
+
+  return (
+    <Container>
+      <SearchMenu pathname={splitPath} location={location} />
+      <SearchControl location={location} search={search} />
+    </Container>
+  );
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  position: relative;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 1;
+  background: #fff;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 20px;
+  box-sizing: border-box;
+  font-size: 15px;
+  flex: 1;
+  box-shadow: inset 0 -1px #d1d1d1;
+  font-family: ${font.en};
+`;
 
 SearchBar.propTypes = {
-  a: PropTypes.shape({
-    b: PropTypes.string
-  })
+  pathname: PropTypes.string
 };
 
 export default SearchBar;
