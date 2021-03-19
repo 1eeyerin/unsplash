af5484a2d4927580b01e8d5c5878da0f499530ad
ye-r1 <ye_r1@daum.net>
2021-03-19 18:05:39 +0900

    #66 검색바 조건 선택에 따라 api 재호출 하도록 qs 추가 반영

--- src/view/containers/SearchPhotoListContainer.js
+++ src/view/containers/SearchPhotoListContainer.js
@@ -6,27 +6,32 @@ import { ContentContainer } from "../../styled/Layout";
 import { useSelector } from "react-redux";
 import InfiniteScroll from "../components/InfiniteScroll";
 import PropTypes from "prop-types";
+import qs from "qs";
 
-function SearchPhotoListContainer({ match }) {
+function SearchPhotoListContainer({ match, location }) {
+  const parsed = qs.parse(location.search, { ignoreQueryPrefix: true });
   const query = match.params.query;
   const { searchResults, isLoading } = useSelector(state => state.search);
   const [page, setPage] = useState(1);
 
   useEffect(() => {
     searchActions.deleteHistory();
-  }, [query]);
+    setPage(1);
+  }, [query, location.search]);
 
   useEffect(() => {
     searchActions.searchPhotos({
       query,
       per_page: 5,
-      page
+      page,
+      ...parsed
     });
-  }, [query, page]);
+  }, [query, page, location.search]);
 
   const getMoreItems = () => {
     if (8 <= page) return;
     setPage(prevPage => prevPage + 1);
+    console.log("@@ page", page);
   };
 
   return (
