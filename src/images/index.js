76d6b443cbdfafb6672270ecc49e8a835f1eb245
ye-r1 <ye_r1@daum.net>
2021-03-25 23:07:13 +0900

    #90 EmptyPhotos => EmptySearch로 변경, collections 추가

--- src/images/index.js
+++ src/images/index.js
@@ -1,3 +1,5 @@
 import EmptyPhotos from "./common/empty_photos.png";
+import EmptyCollections from "./common/empty_collections.png";
 
 export const ImageEmptyPhotos = () => <img src={EmptyPhotos} alt="" />;
+export const ImageEmptyCollections = () => <img src={EmptyCollections} alt="" />;
